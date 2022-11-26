fed418e8dfcf8b07f3dfabbb326c7bd4ae7fa21f
emsifa <emsifa@users.noreply.github.com>
2022-11-26 03:29:02 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,28 +10,32 @@ uses(TestCase::class)->in(__DIR__);
 /**
  * Copy test image so we can use it multiple times
  */
-function create_test_file() {
-    $disk = "test";
-    $original = "sample-image.jpg";
-    $copy = Helper::randomName().".jpg";
+function create_test_file()
+{
+    $disk = 'test';
+    $original = 'sample-image.jpg';
+    $copy = Helper::randomName().'.jpg';
 
     Storage::disk($disk)->copy($original, $copy);
 
     return [$copy, $disk];
 }
 
-function create_test_image_result(): ImageResult {
+function create_test_image_result(): ImageResult
+{
     [$file, $disk] = create_test_file();
+
     return new ImageResult($file, $disk);
 }
 
-function clear_test_files() {
-    $disk = Storage::disk("test");
+function clear_test_files()
+{
+    $disk = Storage::disk('test');
     $files = $disk->files(null, true);
-    $keeps = ["sample-image.jpg"];
+    $keeps = ['sample-image.jpg'];
 
     foreach ($files as $file) {
-        if (!in_array($file, $keeps)) {
+        if (! in_array($file, $keeps)) {
             $disk->delete($file);
         }
     }
