56cc07d89730812994172804e9cc27ffd7132d04
Muhammad Syifa <emsifa@gmail.com>
2022-12-01 18:29:25 +0700

    change datasets to provide params instead object

--- tests/RandomImageTest.php
+++ tests/RandomImageTest.php
@@ -8,17 +8,17 @@ beforeEach(function () {
     History::clear();
 });
 
-it('store image to disk', function (RandomImage $generator) {
-    $result = $generator->store('test-store', 'custom');
+it('store image to disk', function ($params) {
+    $result = (RandomImage::make($params[0], $params[1]))->store('test-store', 'custom');
 
     expect(Storage::disk('custom')->exists($result))->toBeTrue();
     expect(Storage::disk('custom')->get($result))->not->toBeEmpty();
 })
 ->with([
-    'no argument' => RandomImage::make(),
-    'with width' => RandomImage::make(width: 100),
-    'with height' => RandomImage::make(height: 100),
-    'with width and height' => RandomImage::make(100, 50),
+    'no argument' => [[null, null]],
+    'with width' => [[100, null]],
+    'with height' => [[null, 100]],
+    'with width and height' => [[100, 50]],
 ]);
 
 it('get previous image', function () {
